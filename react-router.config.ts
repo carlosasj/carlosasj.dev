3d4919447a6e7613c715d7e2105b46a93ce671da
Carlos Schneider <carlosaschjr@gmail.com>
2026-09-17 00:15:49 +0100

    Setup Github Actions and deployment to Github Pages

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,7 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  // Also render every static route to HTML at build time, so build/client
+  // can be served as-is by GitHub Pages
+  prerender: true,
 } satisfies Config;
